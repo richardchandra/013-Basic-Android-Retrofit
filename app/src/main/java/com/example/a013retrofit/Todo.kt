431f87bb39219a2dfbbe8ef42922fea21cdc9af9
unknown <richardchandra99@gmail.com>
2022-03-17 15:17:08 +0700

    Initial commit for learning HTTP request using retrofit

---
+++ app/src/main/java/com/example/a013retrofit/Todo.kt
@@ -0,0 +1,8 @@
+package com.example.a013retrofit
+
+data class Todo(
+    val completed: Boolean,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
---
+++ app/src/main/java/com/example/a013retrofit/TodoApi.kt
@@ -0,0 +1,12 @@
+package com.example.a013retrofit
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TodoApi {
+
+    @GET("/todos")
+    suspend fun getTodos(): Response<List<Todo>>
+
+}
